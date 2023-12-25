ad77c3ccea4ed11d9794ee957dfdd02a44044470
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-25 22:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const commentsContianer=document.querySelector("#comments-container");
+const textBoxInput=document.querySelector("#textInput");
+const submitBtn=document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click",()=>{
+	if(textBoxInput.value.length!==0){
+		const div=document.createElement("div");
+		const date=new Date();
+		div.innerHTML=`
+						<h1>${textBoxInput.value}</h1>
+							<div class="author">
+								<p>Piyush</p>
+								<p>${date}</p>
+							</div>
+  `
+		commentsContianer.appendChild(div);
+	}
+})
